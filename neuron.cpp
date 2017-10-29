3fb438b0ac02b3792105ac197e34dbed43d69174
marjolaineboulingre <32574159+marjolaineboulingre@users.noreply.github.com>
2017-10-29 19:46:40 +0100

    Add files via upload

--- neuron.cpp
+++ neuron.cpp
@@ -2,54 +2,40 @@
 #include <cmath>
 #include "neuron.hpp" 
 
-Neuron::Neuron(double mp, int t) 
-	: membrane_potential(mp), /*number_spikes(nbrs),*/ last_time_spikes(t)
-	{}
+using namespace std;
 	
 Neuron::Neuron()
- : membrane_potential(0), last_time_spikes(0)
-{}
-		
+	:
+	membrane_potential(0.0),
+	Iext(0.0),
+	delay(1.5),
+	t_spike(0),
+	n_spikes(0),
+	clock(0),
+	spike_buff()
+{
+	delay_steps = static_cast<unsigned long>(ceil(delay/h));
+	spike_buff.resize(delay_steps+1, 0.0);
+	assert(spike_buff.size() == delay_steps + 1);
+}
+	
+
+///getters
+	
 double Neuron::getMembranePotential() const {
 	return membrane_potential;
 }
-		
-/*double Neuron::getNumberSpikes() const {
-	return number_spikes;
-}*/
-		
-		
-void Neuron::setMembranePotential(double mem_pot) {
-	membrane_potential = mem_pot;
-	
-}		
-	
-void Neuron::update(double current, double time)
-{
-	if (isRefractory(time))
-	{
-		setMembranePotential(0);
-	} else {
-		setMembranePotential( exp(-h/tau)*membrane_potential + current*(tau/C)*(1-exp(-h/tau)) + J);
-		potential_historical.push_back(membrane_potential);
-		
-		if (isSpiking()) 
-		{
-			times_historical.push_back(time);
-		}
-	}
+
+double Neuron::getExtCurrent() const {
+	return Iext;
 }
 
-bool Neuron::isSpiking() const {
-	return getMembranePotential() >= Vth;
-		 
+
+unsigned long Neuron::getClock() const {
+	return clock;
 }
 
-bool Neuron::isRefractory(double time) const {
-	return !times_historical.empty() && (time - times_historical.back()) < refractory_time;
-}
-
-double Neuron::getLastSpike() const {
+unsigned long Neuron::getLastTimeSpike() const {
 	if ( !times_historical.empty())
 	 {
 		return times_historical.back();
@@ -57,3 +43,79 @@ double Neuron::getLastSpike() const {
 		return 0;
 	}
 }
+
+double Neuron::getDelay() const {
+	return delay;
+}
+
+int Neuron::getNbrSpikes() const {
+	return n_spikes;
+}
+
+
+///setters
+		
+void Neuron::setMembranePotential(double mem_pot) {
+	membrane_potential = mem_pot;
+}	
+
+void Neuron::setExtCurrent(double I) {
+	Iext = I;
+}	
+	
+	
+///methods concerning the state of the neuron
+
+bool Neuron::isSpiking() const {
+	return getMembranePotential() >= Vth;
+		 
+}
+
+bool Neuron::isRefractory(long c) const {
+	return !times_historical.empty() && (c - times_historical.back()) < refractory_time;
+}
+
+
+void Neuron::updatePotential() {
+		setMembranePotential( c1*getMembranePotential() + Iext*c2 +J);
+}
+	
+
+void Neuron::update(long steps)
+{	
+	const auto t_stop = getClock()+steps;
+	
+	while(getClock() < t_stop)
+	{
+		if (isRefractory(getClock()))
+		{
+			setMembranePotential(0);
+		} else {
+			updatePotential();
+			potential_historical.push_back(membrane_potential);
+		}	
+		
+		if (isSpiking()) 
+		{
+			times_historical.push_back(getClock());
+			++ n_spikes;
+		}
+		
+		spike_buff[ clock % delay_steps] = 0.0;
+		++clock;
+	}
+	
+}
+
+
+void Neuron::receive_spike(unsigned long arrival, double potential) {
+	const size_t t_case = arrival%(delay_steps+ 1);
+	spike_buff[t_case] += potential;
+}
+
+
+double Neuron::convertMs(long c) {
+	return c*h;
+}
+
+
